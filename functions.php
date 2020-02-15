913b7c007df86b6d9846826410bbed4cef51b2ce
Asko Nõmm <asko.nomm@greenpowermonitor.com>
2020-02-15 15:47:35 +0200

    Wrap in a conditional to prevent WP from dying if there's a problem with the plugin

--- functions.php
+++ functions.php
@@ -1,45 +1,47 @@
 <?php
 
-if(!class_exists('Framework')) die('Framework not found.');
+if(class_exists('Framework')) {
 
-require_once __DIR__ . '/app.php';
+	require __DIR__ . '/app.php';
 
-/**
- * Initialize
- */
-$app = new App();
+	/**
+	 * Initialize
+	 */
+	$app = new App();
 
-/**
- * Initialize theme with sensible defaults.
- */
-$app->init();
+	/**
+	 * Initialize theme with sensible defaults.
+	 */
+	$app->init();
 
-/**
- * Register navigation.
- */
-$app->menu('primary', 'Primary Menu');
+	/**
+	 * Register navigation.
+	 */
+	$app->menu( 'primary', 'Primary Menu' );
 
-/**
- * Enqueue scripts and styles.
- */
-$app->style('css', get_stylesheet_uri());
+	/**
+	 * Enqueue scripts and styles.
+	 */
+	$app->style( 'css', get_stylesheet_uri() );
 
-/**
- * Register partials.
- */
-$app->registerTemplatePartials($app->getTemplatePartials());
+	/**
+	 * Register partials.
+	 */
+	$app->registerTemplatePartials( $app->getTemplatePartials() );
 
-/**
- * Register helpers.
- */
-$app->registerTemplateHelpers($app->getTemplateHelpers());
+	/**
+	 * Register helpers.
+	 */
+	$app->registerTemplateHelpers( $app->getTemplateHelpers() );
 
-/**
- * Action!
- */
-$app->action();
+	/**
+	 * Action!
+	 */
+	$app->action();
 
-/**
- * Route!
- */
-$app->route();
+	/**
+	 * Route!
+	 */
+	$app->route();
+
+}
